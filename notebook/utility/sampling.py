412148632d27ad6d2d6e2c5f3fcee1c5befddd40
lzhimin <gokeivn@gmail.com>
2020-02-19 15:59:21 -0700

    add sampling script

---
+++ notebook/utility/sampling.py
@@ -0,0 +1,11 @@
+def activeLearning(boundary, experiments):
+	# The init sample size is 0.005 * len(experiments)
+	# Active learning will sample the rest of 0.005 * len(experiments) in 10 steps
+	# The total number of sample is 0.01
+	# Find the next subset sample.
+	
+
+    pass
+	
+
+
